f3dc11d562ec6d8eee8668d7b480af0d9a26449b
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-13 16:57:57 +0300

    updated

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,39 @@
-import { AppProps } from 'next/app'
 import '@/styles/global.css'
+import { store } from '../store'
+import { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
+import { useEffect, useState } from 'react'
+import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from 'react-toastify'
+import CometChatSSR from '@/components/CometChatNoSSR'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [showChild, setShowChild] = useState(false)
+  useEffect(() => {
+    setShowChild(true)
+  }, [])
+
+  if (!showChild || typeof window === 'undefined') {
+    return null
+  } else {
+    return (
+      <Provider store={store}>
+        <CometChatSSR />
+        <Component {...pageProps} />
+
+        <ToastContainer
+          position="bottom-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </Provider>
+    )
+  }
 }
