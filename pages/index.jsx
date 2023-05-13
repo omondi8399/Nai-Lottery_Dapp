ec0853956e2fc9266f8677b05afc7b370a6887d7
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-05-13 16:46:50 +0300

    Added Index

---
+++ pages/index.jsx
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+import Header from '../components/Header'
+import Jackpots from '../components/Jackpots'
+import { getLotteries } from '@/services/blockchain.srr'
+
+export default function Home({ jackpots }) {
+  return (
+    <div>
+      <Head>
+        <title>Nai Lottery</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div className="min-h-screen bg-slate-100">
+        <Header />
+        <Jackpots jackpots={jackpots} />
+      </div>
+    </div>
+  )
+}
+
+export const getServerSideProps = async () => {
+  const data = await getLotteries()
+  return {
+    props: { jackpots: JSON.parse(JSON.stringify(data)) },
+  }
+}
